595c34738feb22f225c6e908c146dc07f7dcac0a
Stephen <stephen@whitebolt.net>
2017-11-15 15:28:46 +0000

    Do replacements to global space
    First working iteration of code. Will not resolve to root(s) yet.

---
+++ index.js
@@ -0,0 +1,72 @@
+'use strict';
+
+const through = require('through2');
+const path = require('path');
+const fs = require('fs');
+
+const xUrlValues = /\burl\((?:\"|\'|)(.*?)(?:\"|\'|)\)/g;
+const xIsNotLocal = /^data\:|\w+\:\/\//;
+const xQuestionOrHash = /\?|\#/;
+
+function* urls(contents) {
+	let matches;
+	while (matches = xUrlValues.exec(contents)) {
+		let [fullMatch, urlPath] = matches;
+		if (!xIsNotLocal.test(urlPath)) yield [fullMatch, urlPath];
+	}
+}
+
+function trimUrl(urlPath) {
+	return urlPath.split(xQuestionOrHash).shift();
+}
+
+function untrimUrl(trimmedUrl, originalUrl) {
+	return trimmedUrl + originalUrl.replace(trimUrl(originalUrl), '');
+}
+
+function makeArray(ary) {
+	if (Array.isArray(ary)) return ary;
+	if (ary === undefined) return [];
+
+	try {
+		if (Symbol.iterator in ary) return [...ary];
+	} catch (error) {
+		return [ary];
+	}
+}
+
+function getTestPaths(assets, urlPath) {
+	return makeArray(assets).map(assetPath=>path.resolve(assetPath, urlPath));
+}
+
+function getReplacePath(assets, urlPath) {
+	let _urlPath = trimUrl(urlPath);
+	const testPaths = getTestPaths(assets, _urlPath);
+	const found = testPaths.find(urlPath=>fs.existsSync(urlPath));
+	return (found ? untrimUrl(found, urlPath) : urlPath);
+}
+
+function plugin(options, file, encoding, callback) {
+	const ext = file.extname || path.extname(file.path);
+	if ((ext !== '.css') || !file.isBuffer()) return callback(null, file);
+
+	let replaced, contents;
+	contents = file.contents.toString();
+	for (let [fullMatch, urlPath] of urls(contents)) {
+		const replacePath = getReplacePath(options.assets, urlPath);
+		if (replacePath !== urlPath) {
+			replaced = true;
+			contents = contents.replace(fullMatch, fullMatch.replace(urlPath, replacePath));
+		}
+	}
+
+	if (replaced) file.contents = new Buffer(contents);
+	callback(null, file)
+
+}
+
+module.exports = options=>{
+	return through.obj(function (...params){
+		plugin.bind(this)(options, ...params);
+	});
+};
